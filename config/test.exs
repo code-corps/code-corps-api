bf21c1d62e36a1a0199d0e1006636f24532fe771
Josh Smith <joshdotsmith@gmail.com>
2016-07-24 19:53:01 -0700

    Update semaphore settings and remove circle

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :code_corps, CodeCorps.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
   database: "code_corps_phoenix_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
