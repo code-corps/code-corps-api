db424b89ac1363a0a9e8929eb21bdb942d6eff92
Andrew Walton <andrew@greenarrow.me>
2016-08-22 00:15:36 -0400

    Added user_categories migration, model, endpoint (#118)
    * Added user_categories migration, model, endpoint
    
    Addresses #39
    
    * Addressing code review comments.
    
    Added user_categories_view_test
    Added categories relation to user model
    Preloaded categories in all user endpoints

---
+++ priv/repo/migrations/20160822020401_create_user_category.exs
@@ -0,0 +1,14 @@
+defmodule CodeCorps.Repo.Migrations.CreateUserCategory do
+  use Ecto.Migration
+
+  def change do
+    create table(:user_categories) do
+      add :user_id, references(:users, on_delete: :nothing)
+      add :category_id, references(:categories, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:user_categories, [:user_id, :category_id], unique: true)
+  end
+end
