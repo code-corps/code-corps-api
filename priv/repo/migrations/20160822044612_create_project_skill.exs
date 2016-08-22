5a77ef2009983568d350b05937171a5be2e10826
snewcomer <snewcomer24@gmail.com>
2016-08-21 22:25:24 -0700

    [PROJECTSKILL]: WIP - a few commented out tests.  Need to add m2m

---
+++ priv/repo/migrations/20160822044612_create_project_skill.exs
@@ -0,0 +1,15 @@
+defmodule CodeCorps.Repo.Migrations.CreateProjectSkill do
+  use Ecto.Migration
+
+  def change do
+    create table(:project_skills) do
+      add :project_id, references(:projects, on_delete: :nothing)
+      add :skill_id, references(:skills, on_delete: :nothing)
+
+      timestamps()
+    end
+    create index(:project_skills, [:project_id])
+    create index(:project_skills, [:skill_id])
+
+  end
+end
