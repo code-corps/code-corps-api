a8cf414829913cf7cf26489343373ea2b60c1756
Josh Smith <joshdotsmith@gmail.com>
2016-08-22 14:01:12 -0700

    Add members to organization, test organization membership view and organization view

--- web/views/organization_view.ex
+++ web/views/organization_view.ex
@@ -6,6 +6,11 @@ defmodule CodeCorps.OrganizationView do
 
   has_one :slugged_route, serializer: CodeCorps.SluggedRouteView
 
+  has_many :members, serializer: CodeCorps.UserView
+  has_many :organization_memberships,
+    serializer: CodeCorps.OrganizationMembershipView
+  has_many :projects, serializer: CodeCorps.ProjectView
+
   def icon_large_url(organization, _conn) do
     CodeCorps.OrganizationIcon.url({organization.icon, organization}, :large)
   end
