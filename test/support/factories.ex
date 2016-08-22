e0138e0fe40c07d5fbc4e2a197895be3f1673063
Josh Smith <joshdotsmith@gmail.com>
2016-08-22 11:49:37 -0700

    Add skill relationships to user

--- test/support/factories.ex
+++ test/support/factories.ex
@@ -2,6 +2,77 @@ defmodule CodeCorps.Factories do
   # with Ecto
   use ExMachina.Ecto, repo: CodeCorps.Repo
 
+  def category_factory do
+    %CodeCorps.Category{
+      name: sequence(:name, &"Category #{&1}"),
+      slug: sequence(:slug, &"category_#{&1}"),
+      description: sequence(:description, &"A description for category #{&1}"),
+    }
+  end
+
+  def comment_factory do
+    %CodeCorps.Comment{
+      body: "I love elixir!",
+      markdown: "I love elixir!",
+      post: build(:post)
+    }
+  end
+
+  def organization_factory do
+    %CodeCorps.Organization{
+      name: sequence(:username, &"Organization #{&1}"),
+      description: sequence(:email, &"Description of organization #{&1}"),
+    }
+  end
+
+  def organization_membership_factory do
+    %CodeCorps.OrganizationMembership{
+      member: build(:user),
+      organization: build(:organization),
+      role: "contributor"
+    }
+  end
+
+  def post_factory do
+    %CodeCorps.Post{
+      title: "Test post",
+      post_type: "issue",
+      markdown: "A test post",
+      status: "open",
+      project: build(:project),
+      user: build(:user)
+    }
+  end
+
+  def project_factory do
+    %CodeCorps.Project{
+      title: sequence(:title, &"Project #{&1}"),
+      slug: sequence(:slug, &"project_#{&1}")
+    }
+  end
+
+  def project_category_factory do
+    %CodeCorps.ProjectCategory{
+      project: build(:project),
+      category: build(:category)
+    }
+  end
+
+  def role_factory do
+    %CodeCorps.Role{
+      name: sequence(:name, &"Role #{&1}"),
+      ability: sequence(:ability, &"Ability for role #{&1}"),
+      kind: sequence(:kind, &"Kind for role #{&1}")
+    }
+  end
+
+  def skill_factory do
+    %CodeCorps.Skill{
+      description: sequence(:description, &"A description for category #{&1}"),
+      title: sequence(:title, &"Category #{&1}"),
+    }
+  end
+
   def user_factory do
     %CodeCorps.User{
       username: sequence(:username, &"user#{&1}"),
@@ -24,67 +95,10 @@ defmodule CodeCorps.Factories do
     }
   end
 
-  def role_factory do
-    %CodeCorps.Role{
-      name: sequence(:name, &"Role #{&1}"),
-      ability: sequence(:ability, &"Ability for role #{&1}"),
-      kind: sequence(:kind, &"Kind for role #{&1}")
-    }
-  end
-
-  def organization_factory do
-    %CodeCorps.Organization{
-      name: sequence(:username, &"Organization #{&1}"),
-      description: sequence(:email, &"Description of organization #{&1}"),
-    }
-  end
-
-  def organization_membership_factory do
-    %CodeCorps.OrganizationMembership{
-      member: build(:user),
-      organization: build(:organization),
-      role: "contributor"
-    }
-  end
-
-  def project_factory do
-    %CodeCorps.Project{
-      title: sequence(:title, &"Project #{&1}"),
-      slug: sequence(:slug, &"project_#{&1}")
-    }
-  end
-
-  def category_factory do
-    %CodeCorps.Category{
-      name: sequence(:name, &"Category #{&1}"),
-      slug: sequence(:slug, &"category_#{&1}"),
-      description: sequence(:description, &"A description for category #{&1}"),
-    }
-  end
-
-  def project_category_factory do
-    %CodeCorps.ProjectCategory{
-      project: build(:project),
-      category: build(:category)
-    }
-  end
-
-  def post_factory do
-    %CodeCorps.Post{
-      title: "Test post",
-      post_type: "issue",
-      markdown: "A test post",
-      status: "open",
-      project: build(:project),
-      user: build(:user)
-    }
-  end
-
-  def comment_factory do
-    %CodeCorps.Comment{
-      body: "I love elixir!",
-      markdown: "I love elixir!",
-      post: build(:post)
+  def user_skill_factory do
+    %CodeCorps.UserSkill{
+      user: build(:user),
+      skill: build(:skill)
     }
   end
 end
--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -6,11 +6,15 @@ defmodule CodeCorps.UserView do
 
   has_one :slugged_route, serializer: CodeCorps.SluggedRouteView
 
+  has_many :user_categories, serializer: CodeCorps.UserCategoryView
+  has_many :categories, serializer: CodeCorps.CategoryView
+
   has_many :user_roles, serializer: CodeCorps.UserRoleView
   has_many :roles, serializer: CodeCorps.RoleView
 
-  has_many :user_categories, serializer: CodeCorps.UserCategoryView
-  has_many :categories, serializer: CodeCorps.CategoryView
+  has_many :user_skills, serializer: CodeCorps.UserSkillView
+  has_many :skills, serializer: CodeCorps.SkillView
+
 
   def photo_large_url(user, _conn) do
     CodeCorps.UserPhoto.url({user.photo, user}, :large)
