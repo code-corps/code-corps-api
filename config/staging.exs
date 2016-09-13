9c2b99b69c533c448c31cea488c2d3b77fc7b839
Josh Smith <joshdotsmith@gmail.com>
2016-09-12 17:33:25 -0700

    Change host

--- config/staging.exs
+++ config/staging.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :code_corps, CodeCorps.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "pbqrpbecf.org", port: 443],
+  url: [scheme: "https", host: "api.pbqrpbecf.org", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
