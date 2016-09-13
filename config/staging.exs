11d6e2b166b406b044b0584a3b2cb5dc4d6ef578
Josh Smith <joshdotsmith@gmail.com>
2016-09-12 17:00:46 -0700

    Fix prod and staging config

--- config/staging.exs
+++ config/staging.exs
@@ -15,7 +15,7 @@ config :code_corps, CodeCorps.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "code-corps-phoenix-staging.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
