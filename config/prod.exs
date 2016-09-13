30b4509f36c5ff605513afa6d810ced19fe0507f
Josh Smith <joshdotsmith@gmail.com>
2016-09-13 14:12:53 -0700

    Remove static manifest

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,6 @@ config :code_corps, CodeCorps.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "api.codecorps.org", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
