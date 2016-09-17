2c95a81529a7d5d430daf6b6f972a3ec8a9a19c1
Josh Smith <joshdotsmith@gmail.com>
2016-09-17 11:44:04 -0700

    Change static assets to use priv/static and only robots.txt

--- lib/code_corps/endpoint.ex
+++ lib/code_corps/endpoint.ex
@@ -9,7 +9,7 @@ defmodule CodeCorps.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :code_corps, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
