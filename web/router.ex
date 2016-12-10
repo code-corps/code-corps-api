244e8b3d9e460bcdf3eedd4e4c6a801ddb72fbd7
Josh Smith <joshdotsmith@gmail.com>
2016-12-09 21:13:42 -0800

    Fix dupe logging
    Update

--- web/router.ex
+++ web/router.ex
@@ -41,7 +41,7 @@ defmodule CodeCorps.Router do
   end
 
   scope "/", CodeCorps do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:logging, :browser] # Use the default browser stack
 
     get "/", PageController, :index
   end
