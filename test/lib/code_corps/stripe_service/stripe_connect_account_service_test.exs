c665e0da529ae100f5cbe4f75b4076b8aa49d080
Amy Schools <amyschools@gmail.com>
2016-12-15 21:20:05 -0800

    added test file and create test to StripeConnectAccountService

---
+++ test/lib/code_corps/stripe_service/stripe_connect_account_service_test.exs
@@ -0,0 +1,23 @@
+defmodule CodeCorps.StripeService.StripeConnectAccountServiceTest do
+  use ExUnit.Case, async: true
+
+  use CodeCorps.ModelCase
+
+  alias CodeCorps.StripeConnectAccount
+  alias CodeCorps.StripeService.StripeConnectAccountService
+
+  describe "create" do
+    test "creates a StripeConnectAccount" do
+      organization = insert(:organization)
+
+      attributes = %{"country" => "US", "organization_id" => organization.id}
+
+      {:ok, %CodeCorps.StripeConnectAccount{} = connect_account} =
+              StripeConnectAccountService.create(attributes)
+
+      assert connect_account.country == "US"
+      assert connect_account.organization_id == organization.id
+      assert connect_account.managed == true
+    end
+  end
+end
