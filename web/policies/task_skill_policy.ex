fcc3f5d20f66099f533a1e0780e3f0b8c225236e
Nikola Begedin <begedinnikola@gmail.com>
2017-02-01 17:05:15 +0100

    Refactor helpers from UserTask and TaskSkill into Helpers.Policy

--- web/policies/task_skill_policy.ex
+++ web/policies/task_skill_policy.ex
@@ -3,17 +3,25 @@ defmodule CodeCorps.TaskSkillPolicy do
   Represents an authorization policy for performing actions on TaskSkill records.
   Used to authorize a controller action.
   """
-  import CodeCorps.Helpers.Policy,
-    only: [get_project: 1, get_membership: 2, get_role: 1, contributor_or_higher?: 1]
 
-  alias CodeCorps.{Repo, Task, TaskSkill, User}
+  import CodeCorps.Helpers.Policy,
+    only: [
+      contributor_or_higher?: 1,
+      get_membership: 2,
+      get_project: 1,
+      get_role: 1,
+      get_task: 1,
+      task_authored_by?: 2
+    ]
+
+  alias CodeCorps.{TaskSkill, User}
   alias Ecto.Changeset
 
   @spec create?(User.t, Changeset.t) :: boolean
   def create?(%User{} = user, %Changeset{} = changeset) do
     cond do
       changeset |> get_task |> get_project |> get_membership(user) |> get_role |> contributor_or_higher? -> true
-      changeset |> get_task |> authored_by?(user) -> true
+      changeset |> get_task |> task_authored_by?(user) -> true
       true -> false
     end
   end
@@ -22,13 +30,8 @@ defmodule CodeCorps.TaskSkillPolicy do
   def delete?(%User{} = user, %TaskSkill{} = task_skill) do
     cond do
       task_skill |> get_task |> get_project |> get_membership(user) |> get_role |> contributor_or_higher? -> true
-      task_skill |> get_task |> authored_by?(user) -> true
+      task_skill |> get_task |> task_authored_by?(user) -> true
       true -> false
     end
   end
-
-  defp get_task(%TaskSkill{task_id: task_id}), do: Repo.get(Task, task_id)
-  defp get_task(%Changeset{changes: %{task_id: task_id}}), do: Repo.get(Task, task_id)
-
-  defp authored_by?(%Task{user_id: author_id}, %User{id: user_id}), do: user_id == author_id
 end
--- web/policies/user_task_policy.ex
+++ web/policies/user_task_policy.ex
@@ -4,16 +4,23 @@ defmodule CodeCorps.UserTaskPolicy do
   Used to authorize a controller action.
   """
   import CodeCorps.Helpers.Policy,
-    only: [get_project: 1, get_membership: 2, get_role: 1, contributor_or_higher?: 1]
+    only: [
+      contributor_or_higher?: 1,
+      get_membership: 2,
+      get_project: 1,
+      get_role: 1,
+      get_task: 1,
+      task_authored_by?: 2
+    ]
 
-  alias CodeCorps.{Repo, Task, UserTask, User}
+  alias CodeCorps.{User, UserTask}
   alias Ecto.Changeset
 
   @spec create?(User.t, Changeset.t) :: boolean
   def create?(%User{} = user, %Changeset{} = changeset) do
     cond do
       changeset |> get_task |> get_project |> get_membership(user) |> get_role |> contributor_or_higher? -> true
-      changeset |> get_task |> authored_by?(user) -> true
+      changeset |> get_task |> task_authored_by?(user) -> true
       true -> false
     end
   end
@@ -22,13 +29,8 @@ defmodule CodeCorps.UserTaskPolicy do
   def delete?(%User{} = user, %UserTask{} = user_task) do
     cond do
       user_task |> get_task |> get_project |> get_membership(user) |> get_role |> contributor_or_higher? -> true
-      user_task |> get_task |> authored_by?(user) -> true
+      user_task |> get_task |> task_authored_by?(user) -> true
       true -> false
     end
   end
-
-  defp get_task(%UserTask{task_id: task_id}), do: Repo.get(Task, task_id)
-  defp get_task(%Changeset{changes: %{task_id: task_id}}), do: Repo.get(Task, task_id)
-
-  defp authored_by?(%Task{user_id: author_id}, %User{id: user_id}), do: user_id == author_id
 end
