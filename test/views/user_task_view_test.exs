aff42e52d00b30f39272ba536d3233dc6079d6d6
Nikola Begedin <begedinnikola@gmail.com>
2017-02-01 05:39:58 +0100

    Add UsertTaskController, routes, abilities and tests

---
+++ test/views/user_task_view_test.exs
@@ -0,0 +1,32 @@
+defmodule CodeCorps.UserTaskViewTest do
+  @moduledoc false
+
+  use CodeCorps.ViewCase
+
+  test "renders all attributes and relationships properly" do
+    user_task = insert(:user_task)
+
+    rendered_json = render(CodeCorps.UserTaskView, "show.json-api", data: user_task)
+
+    expected_json = %{
+      "data" => %{
+        "id" => user_task.id |> Integer.to_string,
+        "type" => "user-task",
+        "attributes" => %{},
+        "relationships" => %{
+          "task" => %{
+            "data" => %{"id" => user_task.task_id |> Integer.to_string, "type" => "task"}
+          },
+          "user" => %{
+            "data" => %{"id" => user_task.user_id |> Integer.to_string, "type" => "user"}
+          }
+        }
+      },
+      "jsonapi" => %{
+        "version" => "1.0"
+      }
+    }
+
+    assert rendered_json == expected_json
+  end
+end
