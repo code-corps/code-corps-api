bf186fabe416e30c17e13b909c3d750b69898f09
snewcomer <snewcomer24@gmail.com>
2017-03-08 15:03:27 -0800

    [AUTH TOKEN]: model and test that value was created

---
+++ priv/repo/migrations/20170308222552_create_auth_token.exs
@@ -0,0 +1,15 @@
+defmodule CodeCorps.Repo.Migrations.CreateAuthToken do
+  use Ecto.Migration
+
+  def change do
+    create table(:auth_token) do
+      add :value, :string
+
+      add :user_id, references(:users, on_delete: :delete_all)
+
+      timestamps()
+    end
+
+    create index(:auth_token, [:user_id])
+  end
+end
---
+++ web/models/auth_token.ex
@@ -0,0 +1,31 @@
+defmodule CodeCorps.AuthToken do
+  @moduledoc """
+  Represent's one of the user's many possible authentication tokens, created
+  using `Phoenix.Token.sign/4`.
+
+  Many can coexist and be valid at the same time. They can be used for password
+  resets or passwordless logins.
+
+  These tokens expire.
+  """
+
+  use CodeCorps.Web, :model
+
+  schema "auth_token" do
+    field :value, :string
+
+    belongs_to :user, CodeCorps.User
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct`
+  """
+  def changeset(struct, user) do
+    token = CodeCorps.Endpoint |> Phoenix.Token.sign("user", user.id)
+    struct
+    |> cast(%{ value: token }, [:value])
+    |> validate_required([:value])
+  end
+end
