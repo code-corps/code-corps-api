d21f8b4f16aa3d61ba5da151acaa5c0c6549c31a
npendery <npendery@gmail.com>
2017-04-25 11:59:21 -0600

    Add /github-connect route and action - adds route - adds user controller action - adds api docs for action
    Action
    - calls placeholder method in github module
    - responds with user if ok
    - responds with 422 if code param is bad
    - responds with 500 if something else goes wrong

--- lib/code_corps/github.ex
+++ lib/code_corps/github.ex
@@ -2,6 +2,11 @@ defmodule CodeCorps.Github do
 
   alias CodeCorps.{User, Repo}
 
+  @doc """
+  Temporary function until the actual behavior is implemented.
+  """
+  def connect(user, _code), do: {:ok, user}
+
   def associate(user, params) do
     user
     |> User.github_associate_changeset(params)
