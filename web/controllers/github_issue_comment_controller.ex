0dfaa3755b42f3e31ededaf74cb917aa4c50dfd6
npendery <npendery@gmail.com>
2017-04-28 14:16:15 -0600

    Create issue comment controller

---
+++ web/controllers/github_issue_comment_controller.ex
@@ -0,0 +1,56 @@
+defmodule CodeCorps.GithubIssueCommentController do
+  use CodeCorps.Web, :controller
+
+  alias CodeCorps.Comment
+
+  def handle(conn, payload) do
+    case payload["action"] do
+      "created" ->
+        # create comment
+        attributes = convert_comment_attributes(payload)
+        changeset = %Comment{} |> Comment.create_changeset(attributes)
+        case Repo.insert(changeset) do
+          {:ok, _comment} ->
+            conn
+          {:error, changeset}
+            # log error
+            conn
+        end
+      "edited" ->
+        # update comment
+        comment = lookup_comment(payload)
+        attributes = convert_comment_attributes(payload)
+        changeset = comment |> Comment.changeset(attributes)
+        case Repo.update(changeset) do
+          {:ok, _comment} ->
+            conn
+          {:error, changeset}
+            # log error
+            conn
+        end
+      "deleted" ->
+        # delete comment
+        comment = lookup_comment(payload)
+        Repo.delete(comment)
+        conn
+      _ ->
+        # log error or do nothing
+        conn
+    end
+  end
+
+  defp lookup_comment(payload) do
+    comment_id = payload["comment"]["id"]
+    Comment |> Repo.get_by(github_id: comment_id)
+  end
+
+  defp convert_comment_attributes(payload) do
+    comment = payload["comment"]
+    %{
+      "github_id" => comment["id"],
+      "markdown" => comment["body"],
+      "task_id" => CodeCorps.Task |> Repo.get_by(github_id: payload["issue"]["id"]),
+      "user_id" => CodeCorps.User |> Repo.get_by(github_id: comment["user"]["id"]) # Need to add funtion to create random CC user if no user in system
+    }
+  end
+end
