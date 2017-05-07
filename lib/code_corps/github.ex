5b326429ba420905ab2014da76e484240e1e8a40
npendery <npendery@gmail.com>
2017-05-07 17:18:58 -0600

    Update default user token on github

--- lib/code_corps/github.ex
+++ lib/code_corps/github.ex
@@ -227,7 +227,7 @@ defmodule CodeCorps.GitHub do
   end
 
   def create_issue(attributes, project, current_user) do
-    access_token = current_user.access_token #|| default_user_token # need to create the default user
+    access_token = current_user.github_access_token || default_user_token() # need to create the Github user for this token
     client = Tentacat.Client.new(%{access_token: access_token})
     response = Tentacat.Issues.create(
       project.github_owner,
@@ -249,5 +249,9 @@ defmodule CodeCorps.GitHub do
       title: attributes[:title],
       body: attributes[:body]
     }
+  end
+
+  defp default_user_token do
+    System.get_env("GITHUB_DEFAULT_USER_TOKEN")
   end
 end
