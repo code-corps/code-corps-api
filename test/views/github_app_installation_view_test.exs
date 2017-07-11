84712d53ab20a6c522ae2f62c77d487a7a692913
Josh Smith <joshdotsmith@gmail.com>
2017-07-11 14:42:38 -0700

    Add github_repos relationship to installation view and test

--- test/views/github_app_installation_view_test.exs
+++ test/views/github_app_installation_view_test.exs
@@ -7,6 +7,7 @@ defmodule CodeCorps.GithubAppInstallationViewTest do
     user = insert(:user)
     github_app_installation = insert(:github_app_installation, project: project, user: user)
     organization_github_app_installation = insert(:organization_github_app_installation, github_app_installation: github_app_installation, organization: organization)
+    github_repo = insert(:github_repo, github_app_installation: github_app_installation)
 
     rendered_json = render(CodeCorps.GithubAppInstallationView, "show.json-api", data: github_app_installation)
 
@@ -22,6 +23,11 @@ defmodule CodeCorps.GithubAppInstallationViewTest do
           "updated-at" => github_app_installation.updated_at
         },
         "relationships" => %{
+          "github-repos" => %{
+            "data" => [
+              %{"id" => github_repo.id |> Integer.to_string, "type" => "github-repo"}
+            ]
+          },
           "organization-github-app-installations" => %{
             "data" => [
               %{"id" => organization_github_app_installation.id |> Integer.to_string, "type" => "organization-github-app-installation"}
