d8340b1dfd1832d4f6257fb3acbbd2d5b57c62a4
Nikola Begedin <begedinnikola@gmail.com>
2017-07-28 17:20:18 +0200

    Added process to associate connected github user with matching orphaned installations

--- test/lib/code_corps/github/user_test.exs
+++ test/lib/code_corps/github/user_test.exs
@@ -4,7 +4,7 @@ defmodule CodeCorps.GitHub.UserTest do
   use CodeCorps.ModelCase
   use CodeCorps.GitHubCase
 
-  alias CodeCorps.{GitHub, User}
+  alias CodeCorps.{GitHub, GithubAppInstallation, User}
 
   @user_data %{
     "avatar_url" => "foo_url",
@@ -17,8 +17,16 @@ defmodule CodeCorps.GitHub.UserTest do
 
   describe "connect/2" do
     @tag bypass: %{"/user" => {200, @user_data}, "/" => {200, @token_data}}
-    test "posts to github, updates user if reply is ok, returns updated user" do
+    test "posts to github, associates user and installations, returns updated user" do
       user = insert(:user)
+
+      # 3 test installations
+      # this one should associate, because it's orphaned and matches github id
+      installation_1 = insert(:github_app_installation, user: nil, sender_github_id: 123)
+      # this one matches github id, but is not orphaned, so should not associate
+      installation_2 = insert(:github_app_installation, sender_github_id: 123)
+      # this one is orphaned, but does not match github id, should not associate
+      installation_3 = insert(:github_app_installation, user: nil, sender_github_id: 234)
 
       {:ok, %User{} = returned_user} = GitHub.User.connect(user, "foo_code", "foo_state")
 
@@ -28,6 +36,12 @@ defmodule CodeCorps.GitHub.UserTest do
       assert returned_user.email == "foo_email"
       assert returned_user.github_id == 123
       assert returned_user.github_username == "foo_login"
+
+      assert Enum.count(returned_user.github_app_installations) == 1
+
+      assert Repo.get(GithubAppInstallation, installation_1.id).user_id == returned_user.id
+      refute Repo.get(GithubAppInstallation, installation_2.id).user_id == returned_user.id
+      refute Repo.get(GithubAppInstallation, installation_3.id).user_id == returned_user.id
     end
 
     @error_data %{"error" => "Not Found"}
