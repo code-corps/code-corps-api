89316bac4417857d237a75ae22ddaa0d5f4ea20f
crodriguez1a <crodriguez1a@gmail.com>
2017-08-31 08:56:29 -0400

    Attempt to fix commit history

--- lib/code_corps/policy/category.ex
+++ lib/code_corps/policy/category.ex
@@ -1,9 +1,11 @@
 defmodule CodeCorps.Policy.Category do
-  alias CodeCorps.User
-
+  alias CodeCorps.{ User }
+  
+  @spec create?(User.t) :: boolean
   def create?(%User{admin: true}), do: true
-  def create?(%User{admin: false}), do: false
+  def create?(%User{}), do: false
 
+  @spec update?(User.t) :: boolean
   def update?(%User{admin: true}), do: true
-  def update?(%User{admin: false}), do: false
+  def update?(%User{}), do: false
 end
