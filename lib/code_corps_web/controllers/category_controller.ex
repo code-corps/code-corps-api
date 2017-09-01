798690be84afef81650111c08b9989bb47436a53
Carlos Rodriguez <crodriguez1a@gmail.com>
2017-09-01 08:16:48 -0400

    Switches category controller away from ja_resource  (#881)
    * Swithed category controller away from ja_resource and canary
    
    * Address comments

--- lib/code_corps_web/controllers/category_controller.ex
+++ lib/code_corps_web/controllers/category_controller.ex
@@ -1,17 +1,43 @@
 defmodule CodeCorpsWeb.CategoryController do
   use CodeCorpsWeb, :controller
-  use JaResource
 
-  alias CodeCorps.Category
+  alias CodeCorps.{Category, User, Helpers.Query}
 
-  plug :load_resource, model: Category, only: [:show]
-  plug :load_and_authorize_resource, model: Category, only: [:create, :update]
-  plug JaResource
+  action_fallback CodeCorpsWeb.FallbackController
+  plug CodeCorpsWeb.Plug.DataToAttributes
 
-  @spec model :: module
-  def model, do: CodeCorps.Category
+  @spec index(Conn.t, map) :: Conn.t
+  def index(%Conn{} = conn, %{} = params) do
+    with categories <- Category |> Query.id_filter(params) |> Repo.all do
+      conn |> render("index.json-api", data: categories)
+    end
+  end
 
-  def handle_create(_conn, attributes) do
-    Category.create_changeset(%Category{}, attributes)
+  @spec show(Conn.t, map) :: Conn.t
+  def show(%Conn{} = conn, %{"id" => id}) do
+    with %Category{} = category <- Category |> Repo.get(id) do
+      conn |> render("show.json-api", data: category)
+    end
+  end
+
+  @spec create(Conn.t, map) :: Conn.t
+  def create(%Conn{} = conn, %{} = params) do
+    with %User{} = current_user <- conn |> Guardian.Plug.current_resource,
+      {:ok, :authorized} <- current_user |> Policy.authorize(:create, %Category{}, params),
+      {:ok, %Category{} = category} <- %Category{} |> Category.create_changeset(params) |> Repo.insert
+    do
+      conn |> put_status(:created) |> render("show.json-api", data: category)
+    end
+  end
+
+  @spec update(Conn.t, map) :: Conn.t
+  def update(%Conn{} = conn, %{"id" => id} = params) do
+    with %Category{} = category <- Category |> Repo.get(id),
+      %User{} = current_user <- conn |> Guardian.Plug.current_resource,
+      {:ok, :authorized} <- current_user |> Policy.authorize(:update, category),
+      {:ok, %Category{} = category} <- category |> Category.changeset(params) |> Repo.update
+    do
+      conn |> render("show.json-api", data: category)
+    end
   end
 end
