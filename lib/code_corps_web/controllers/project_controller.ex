935a4150418fb0a0afefbfc9957a5c4f3681c8e3
Nikola Begedin <begedinnikola@gmail.com>
2017-09-14 16:17:47 +0200

    Cleaned up ProjectController, introduced Project.Query

--- lib/code_corps_web/controllers/project_controller.ex
+++ lib/code_corps_web/controllers/project_controller.ex
@@ -1,36 +1,21 @@
 defmodule CodeCorpsWeb.ProjectController do
   use CodeCorpsWeb, :controller
 
-  alias CodeCorps.{Helpers.Query, Project, User}
+  alias CodeCorps.{Project, User}
 
   action_fallback CodeCorpsWeb.FallbackController
-  plug CodeCorpsWeb.Plug.DataToAttributes  
+  plug CodeCorpsWeb.Plug.DataToAttributes
 
   @spec index(Conn.t, map) :: Conn.t
-  def index(%Conn{} = conn, _params) do
-    with projects <- Project |> Query.approved_filter(true) |> Repo.all do
-      conn |> render("index.json-api", data: projects)
-    end
-  end
-
-  @spec index(Conn.t, map) :: Conn.t
-  def index(%Conn{} = conn, %{"slug" => slug}) do
-    slugged_route = CodeCorps.SluggedRoute |> Query.slug_finder(slug)
-    with projects <- Project |> Repo.all(organization_id: slugged_route.organization_id) do
+  def index(%Conn{} = conn, %{} = params) do
+    with projects <- Project.Query.list(params) do
       conn |> render("index.json-api", data: projects)
     end
   end
 
   @spec show(Conn.t, map) :: Conn.t
-  def show(%Conn{} = conn, %{"id" => id}) do
-    with %Project{} = project <- Project |> Repo.get(id) do
-      conn |> render("show.json-api", data: project)
-    end
-  end
-
-  @spec show(Conn.t, map) :: Conn.t
-  def show(%Conn{} = conn, %{"project_slug" => project_slug}) do
-    with %Project{} = project <- Project |> Query.slug_finder(project_slug) do
+  def show(%Conn{} = conn, %{} = params) do
+    with %Project{} = project <- Project.Query.find(params) do
       conn |> render("show.json-api", data: project)
     end
   end
@@ -45,8 +30,8 @@ defmodule CodeCorpsWeb.ProjectController do
   end
 
   @spec update(Conn.t, map) :: Conn.t
-  def update(%Conn{} = conn, %{"id" => id} = params) do
-    with %Project{} = project <- Project |> Repo.get(id),
+  def update(%Conn{} = conn, %{} = params) do
+    with %Project{} = project <- Project.Query.find(params),
       %User{} = current_user <- conn |> Guardian.Plug.current_resource,
       {:ok, :authorized} <- current_user |> Policy.authorize(:update, project),
       {:ok, %Project{} = project} <- project |> Project.changeset(params) |> Repo.update do
