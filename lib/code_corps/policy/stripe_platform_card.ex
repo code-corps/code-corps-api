2cb10dda2e0f36ee7daed7d75fb9353121b4eb37
Landon Grindheim <landon.grindheim@gmail.com>
2017-10-09 08:28:16 -0400

    Remove JaResource/Canary from StripePlatformCardController (#1021)

--- lib/code_corps/policy/stripe_platform_card.ex
+++ lib/code_corps/policy/stripe_platform_card.ex
@@ -1,17 +1,19 @@
 defmodule CodeCorps.Policy.StripePlatformCard do
   alias CodeCorps.StripePlatformCard
   alias CodeCorps.User
-  alias Ecto.Changeset
 
-  def create?(user, card), do: user |> owns?(card)
-  def delete?(user, changeset), do: user |> owns?(changeset)
+  @spec create?(User.t, map) :: boolean
+  def create?(user, params), do: user |> owns?(params)
+
+  @spec show?(User.t, StripePlatformCard.t) :: boolean
   def show?(user, card), do: user |> owns?(card)
 
-  defp owns?(%User{id: current_user_id}, %Changeset{changes: %{user_id: user_id}}) do
+  @spec owns?(User.t, StripePlatformCard.t | map) :: boolean
+  defp owns?(%User{id: current_user_id}, %StripePlatformCard{user_id: user_id}) do
     current_user_id == user_id
   end
 
-  defp owns?(%User{id: current_user_id}, %StripePlatformCard{user_id: user_id}) do
+  defp owns?(%User{id: current_user_id}, %{"user_id" => user_id}) do
     current_user_id == user_id
   end
 
