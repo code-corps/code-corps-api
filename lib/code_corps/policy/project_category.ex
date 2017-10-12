5ea81ef9ff4db8ef5cf67f5f161dbadd3414cd6a
Landon Grindheim <landon.grindheim@gmail.com>
2017-10-11 20:23:47 -0400

    Remove JaReource from ProjectCategoryController

--- lib/code_corps/policy/project_category.ex
+++ lib/code_corps/policy/project_category.ex
@@ -2,12 +2,13 @@ defmodule CodeCorps.Policy.ProjectCategory do
   import CodeCorps.Policy.Helpers, only: [get_project: 1, administered_by?: 2]
 
   alias CodeCorps.{ProjectCategory, User}
-  alias Ecto.Changeset
 
-  def create?(%User{} = user, %Changeset{} = changeset) do
-    changeset |> get_project |> administered_by?(user)
+  @spec create?(User.t, map) :: boolean
+  def create?(%User{} = user, %{} = params) do
+    params |> get_project |> administered_by?(user)
   end
 
+  @spec delete?(User.t, ProjectCategory.t) :: boolean
   def delete?(%User{} = user, %ProjectCategory{} = project_category) do
     project_category |> get_project |> administered_by?(user)
   end
--- lib/code_corps_web/controllers/project_category_controller.ex
+++ lib/code_corps_web/controllers/project_category_controller.ex
@@ -1,24 +1,43 @@
 defmodule CodeCorpsWeb.ProjectCategoryController do
   use CodeCorpsWeb, :controller
-  use JaResource
 
-  alias CodeCorps.ProjectCategory
+  alias CodeCorps.{ProjectCategory, User, Helpers.Query}
 
-  import CodeCorps.Helpers.Query, only: [id_filter: 2]
+  action_fallback CodeCorpsWeb.FallbackController
+  plug CodeCorpsWeb.Plug.DataToAttributes
+  plug CodeCorpsWeb.Plug.IdsToIntegers
 
-  plug :load_resource, model: ProjectCategory, only: [:show], preload: [:project, :category]
-  plug :load_and_authorize_changeset, model: ProjectCategory, only: [:create]
-  plug :load_and_authorize_resource, model: ProjectCategory, only: [:delete]
-  plug JaResource
-
-  @spec model :: module
-  def model, do: CodeCorps.ProjectCategory
-
-  def filter(_conn, query, "id", id_list) do
-    query |> id_filter(id_list)
+  @spec index(Conn.t, map) :: Conn.t
+  def index(%Conn{} = conn, %{} = params) do
+    with project_categories <- ProjectCategory |> Query.id_filter(params) |> Repo.all do
+      conn |> render("index.json-api", data: project_categories)
+    end
   end
 
-  def handle_create(_conn, attributes) do
-    %ProjectCategory{} |> ProjectCategory.create_changeset(attributes)
+  @spec show(Conn.t, map) :: Conn.t
+  def show(%Conn{} = conn, %{"id" => id}) do
+    with %ProjectCategory{} = project_category <- ProjectCategory |> Repo.get(id) do
+      conn |> render("show.json-api", data: project_category)
+    end
+  end
+
+  @spec create(Plug.Conn.t, map) :: Conn.t
+  def create(%Conn{} = conn, %{} = params) do
+    with %User{} = current_user <- conn |> Guardian.Plug.current_resource,
+         {:ok, :authorized} <- current_user |> Policy.authorize(:create, %ProjectCategory{}, params),
+         {:ok, %ProjectCategory{} = project_category} <- %ProjectCategory{} |> ProjectCategory.create_changeset(params) |> Repo.insert do
+      conn |> put_status(:created) |> render("show.json-api", data: project_category)
+    end
+  end
+
+  @spec delete(Conn.t, map) :: Conn.t
+  def delete(%Conn{} = conn, %{"id" => id} = _params) do
+    with %ProjectCategory{} = project_category <- ProjectCategory |> Repo.get(id),
+      %User{} = current_user <- conn |> Guardian.Plug.current_resource,
+      {:ok, :authorized} <- current_user |> Policy.authorize(:delete, project_category),
+      {:ok, %ProjectCategory{} = _project_category} <- project_category |> Repo.delete
+    do
+      conn |> Conn.assign(:project_category, project_category) |> send_resp(:no_content, "")
+    end
   end
 end
