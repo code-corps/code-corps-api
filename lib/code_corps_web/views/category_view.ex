e295bf2d9a6af25d0c3bc2b8fbfc04cda0a445d0
Scott Newcomer <snewcomer24@gmail.com>
2017-11-01 15:17:00 -0700

    add jsonapi to project and point to master

--- lib/code_corps_web/views/category_view.ex
+++ lib/code_corps_web/views/category_view.ex
@@ -1,13 +1,12 @@
 defmodule CodeCorpsWeb.CategoryView do
   @moduledoc false
   use CodeCorpsWeb, :view
-  use JSONAPI.View
+  use JSONAPI.View, type: "category"
 
   def render("show.json-api", %{ data: category, conn: conn, params: params }) do
     __MODULE__.show(category, conn, params)
   end
 
   def fields, do: [:name, :slug, :description]
-  def type, do: "category"
   def relationships, do: [project_categories: CodeCorpsWeb.ProjectCategoryView]
 end
--- lib/code_corps_web/views/project_category_view.ex
+++ lib/code_corps_web/views/project_category_view.ex
@@ -1,14 +1,9 @@
 defmodule CodeCorpsWeb.ProjectCategoryView do
   @moduledoc false
   use CodeCorpsWeb, :view
-  use JSONAPI.View
-
-  def render("show.json", %{data: project_category, conn: conn, params: params}) do
-    __MODULE__.show(project_category, conn, params)
-  end
+  use JSONAPI.View, type: "project-category"
 
   def fields, do: []
-  def type, do: "project-category"
   def relationships, do: [project: CodeCorpsWeb.ProjectView,
     category: CodeCorpsWeb.CategoryView]
 end
