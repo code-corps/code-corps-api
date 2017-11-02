ed5817860540a08a9c8cf974f453d1c06b3c6103
Scott Newcomer <snewcomer24@gmail.com>
2017-11-01 18:45:00 -0700

    update benchit with view

--- priv/repo/benchit.exs
+++ priv/repo/benchit.exs
@@ -1,12 +1,15 @@
 alias CodeCorps.{
   Category,Repo
 }
+import Phoenix.View, only: [render: 3]
 
-category = %Category{
-    id: 12
-}
+# category = %Category{
+#     id: 12
+# }
+
+category = Category |> Repo.get(12) |> Repo.preload([:project_categories])
 
 Benchee.run(%{
-    "jsonapi"    => fn -> CodeCorpsWeb.CategoryController.show_with_jsonapi(%Plug.Conn{}, %{"id" => category.id}) end,
-    # "existing"   => fn -> CodeCorpsWeb.CategoryController.show(%Plug.Conn{}, %{"id" => category.id}) end
+    "jsonapi"    => fn -> render(CodeCorpsWeb.CategoryjsonapiView, "show.json-api", %{ data: category, conn: nil, params: category.id }) end,
+    "existing"   => fn -> render(CodeCorpsWeb.CategoryView, "show.json-api", data: category) end
 }, time: 10)
