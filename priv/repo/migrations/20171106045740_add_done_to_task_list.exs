8e4cf298a98a4ac47be2684f56e9b2d0998299be
Josh Smith <josh@codecorps.org>
2017-11-06 12:51:46 -0800

    Fix migration for adding done to task lists

--- priv/repo/migrations/20171106045740_add_done_to_task_list.exs
+++ priv/repo/migrations/20171106045740_add_done_to_task_list.exs
@@ -18,15 +18,17 @@ defmodule CodeCorps.Repo.Migrations.AddDoneToTaskList do
     task_list_query =
       from(tl in "task_lists", where: [done: true], select: [:id])
 
-    # tests do not have any data, so we need to account for potential nil
-    case task_list_query |> Repo.one do
-      %{id: done_list_id} ->
-        task_update_query = from t in "tasks",
-          where: [status: "closed"],
-          update: [set: [task_list_id: ^done_list_id]]
-        task_update_query |> Repo.update_all([])
-      nil -> nil
-    end
+    task_list_query |> Repo.all() |> Enum.each(fn task ->
+      # tests do not have any data, so we need to account for potential nil
+      case task do
+        %{id: done_list_id} ->
+          task_update_query = from t in "tasks",
+            where: [status: "closed"],
+            update: [set: [task_list_id: ^done_list_id]]
+          task_update_query |> Repo.update_all([])
+        nil -> nil
+      end
+    end)
   end
 
   def down do
