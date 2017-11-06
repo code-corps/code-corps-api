9c69e3a88ac7304e50366e075a735bdd8002213f
Josh Smith <josh@codecorps.org>
2017-11-05 21:24:48 -0800

    Load task lists progressively

--- lib/code_corps_web/views/task_list_view.ex
+++ lib/code_corps_web/views/task_list_view.ex
@@ -7,5 +7,5 @@ defmodule CodeCorpsWeb.TaskListView do
 
   has_one :project, type: "project", field: :project_id
 
-  has_many :tasks, serializer: CodeCorpsWeb.TaskInListView, include: true
+  has_many :tasks, serializer: CodeCorpsWeb.TaskView, identifiers: :always
 end
