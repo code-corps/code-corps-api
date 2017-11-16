592e28bdd165bfab72b8622cb3fef7a4c8cbea03
Josh Smith <josh@codecorps.org>
2017-11-16 11:32:55 -0800

    Fix modified_at comparisons to use seconds granularity

--- lib/code_corps/validators/time_validator.ex
+++ lib/code_corps/validators/time_validator.ex
@@ -7,20 +7,32 @@ defmodule CodeCorps.Validators.TimeValidator do
 
   @doc """
   Validates the new time is not before the previous time.
+
+  Works at second-level accuracy by truncating both timestamps to the second.
   """
   def validate_time_not_before(%{data: data} = changeset, field) do
     previous_time = Map.get(data, field)
-    current_time = Changeset.get_change(changeset, field)
-    case current_time do
+    new_time = Changeset.get_change(changeset, field)
+    case new_time do
       nil -> changeset
-      _ -> do_validate_time_not_before(changeset, field, previous_time, current_time)
+      _ -> do_validate_time_not_before(changeset, field, previous_time, new_time)
     end
   end
 
-  defp do_validate_time_not_before(changeset, field, previous_time, current_time) do
-    case Timex.before?(current_time, previous_time) do
+  defp do_validate_time_not_before(changeset, field, previous_time, new_time) do
+    previous_time = previous_time |> truncate(:second)
+    new_time = new_time |> truncate(:second)
+    case Timex.before?(new_time, previous_time) do
       true -> Changeset.add_error(changeset, field, "cannot be before the last recorded time")
       false -> changeset
     end
   end
+
+  # TODO: Replace this with DateTime.truncate/2 when Elixir 1.6 releases
+  @spec truncate(DateTime.t, :microsecond | :millisecond | :second) :: DateTime.t
+  def truncate(%DateTime{microsecond: microsecond} = datetime, precision) do
+    %{datetime | microsecond: do_truncate(microsecond, precision)}
+  end
+
+  defp do_truncate(_, :second), do: {0, 0}
 end
