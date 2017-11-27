6f9264f2774eb64747a0eb4e9957864efa68233d
Nikola Begedin <begedinnikola@gmail.com>
2017-11-27 15:36:05 +0100

    Added filter by code support to org invites

--- lib/code_corps/helpers/query.ex
+++ lib/code_corps/helpers/query.ex
@@ -64,4 +64,22 @@ defmodule CodeCorps.Helpers.Query do
   end
 
   # end finders
+
+  @doc ~S"""
+  Applies optional filters by key-value to query dynamically.
+
+  Used by piping a queryable with a map of parameters and a list of keys to
+  filter by.
+
+  For each key in the list, the params map has a value for that key,
+  the query condition for that `{key, value}` is applied to the queriable.
+  """
+  @spec optional_filters(Queryable.t, map, list) :: Queryable.t
+  def optional_filters(query, %{} = params, [key | other_keys]) do
+    case params |> Map.get(key |> Atom.to_string) do
+      nil -> query |> optional_filters(params, other_keys)
+      value -> query |> where([o], field(o, ^key) == ^value)
+    end
+  end
+  def optional_filters(query, %{} = params , []), do: query
 end
