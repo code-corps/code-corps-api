bf72b97edc0172fd0db5f04bf467371318dae984
Nikola Begedin <begedinnikola@gmail.com>
2017-12-29 18:37:33 +0100

    Use ProjectUser as provider of valid roles for UserInvite validation

--- lib/code_corps/accounts/user_invites.ex
+++ lib/code_corps/accounts/user_invites.ex
@@ -4,26 +4,30 @@ defmodule CodeCorps.Accounts.UserInvites do
 
   import Ecto.Query
 
-  @spec create_invite(map) :: {:ok, UserInvite.t} | {:error, Changeset.t}
+  @spec create_invite(map) :: {:ok, UserInvite.t()} | {:error, Changeset.t()}
   def create_invite(%{} = params) do
     %UserInvite{}
     |> Changeset.cast(params, [:email, :name, :role, :inviter_id, :project_id])
     |> Changeset.validate_required([:email, :inviter_id])
-    |> Changeset.validate_inclusion(:role, ~w(contributor admin owner))
+    |> Changeset.validate_inclusion(:role, ProjectUser.roles())
     |> Changeset.assoc_constraint(:inviter)
     |> Changeset.assoc_constraint(:project)
     |> ensure_email_not_owned_by_member()
-    |> Repo.insert
+    |> Repo.insert()
   end
 
-  @spec ensure_email_not_owned_by_member(Changeset.t) :: Changeset.t
+  @spec ensure_email_not_owned_by_member(Changeset.t()) :: Changeset.t()
   defp ensure_email_not_owned_by_member(%Changeset{} = changeset) do
     email = changeset |> Changeset.get_change(:email)
     project_id = changeset |> Changeset.get_change(:project_id)
 
     case [email, project_id] do
-      [nil, _] -> changeset
-      [_, nil] -> changeset
+      [nil, _] ->
+        changeset
+
+      [_, nil] ->
+        changeset
+
       [email, project_id] ->
         count =
           ProjectUser
@@ -31,7 +35,7 @@ defmodule CodeCorps.Accounts.UserInvites do
           |> join(:inner, [pu], u in User, pu.user_id == u.id)
           |> where([_pu, u], u.email == ^email)
           |> select([pu, _U], count(pu.id))
-          |> Repo.one
+          |> Repo.one()
 
         if count > 0 do
           changeset |> Changeset.add_error(:email, "Already associated with a project member")
@@ -41,9 +45,9 @@ defmodule CodeCorps.Accounts.UserInvites do
     end
   end
 
-  @spec claim_invite(map) :: {:ok, User.t}
+  @spec claim_invite(map) :: {:ok, User.t()}
   def claim_invite(%{} = params) do
-    Multi.new
+    Multi.new()
     |> Multi.run(:load_invite, fn %{} -> params |> load_invite() end)
     |> Multi.run(:user, fn %{} -> params |> claim_new_user() end)
     |> Multi.run(:project_user, fn %{user: user, load_invite: user_invite} ->
@@ -52,45 +56,48 @@ defmodule CodeCorps.Accounts.UserInvites do
     |> Multi.run(:user_invite, fn %{user: user, load_invite: user_invite} ->
       user_invite |> associate_invitee(user)
     end)
-    |> Repo.transaction
+    |> Repo.transaction()
     |> marshall_response()
   end
 
-  @spec load_invite(map) :: {:ok, UserInvite.t} | {:error, :not_found}
+  @spec load_invite(map) :: {:ok, UserInvite.t()} | {:error, :not_found}
   defp load_invite(%{"invite_id" => invite_id}) do
     case UserInvite |> Repo.get(invite_id) |> Repo.preload([:invitee, :project]) do
       nil -> {:error, :not_found}
       %UserInvite{} = invite -> {:ok, invite}
     end
   end
+
   defp load_invite(%{}), do: {:error, :not_found}
 
-  @spec claim_new_user(map) :: {:ok, User.t}
+  @spec claim_new_user(map) :: {:ok, User.t()}
   defp claim_new_user(%{} = params) do
-    %User{} |> User.registration_changeset(params) |> Repo.insert
+    %User{} |> User.registration_changeset(params) |> Repo.insert()
   end
 
-  @spec join_project(User.t, UserInvite.t) :: {:ok, ProjectUser.t} | {:error, Changeset.t}
+  @spec join_project(User.t(), UserInvite.t()) :: {:ok, ProjectUser.t()} | {:error, Changeset.t()}
   defp join_project(%User{} = user, %UserInvite{role: role, project: %Project{} = project}) do
     case ProjectUser |> Repo.get_by(user_id: user.id, project_id: project.id) do
       %ProjectUser{} = project_user ->
         {:ok, project_user}
+
       nil ->
         %ProjectUser{}
         |> Changeset.change(%{role: role})
         |> Changeset.put_assoc(:project, project)
         |> Changeset.put_assoc(:user, user)
-        |> Repo.insert
+        |> Repo.insert()
     end
   end
+
   defp join_project(%User{}, %UserInvite{}), do: {:ok, nil}
 
-  @spec associate_invitee(UserInvite.t, User.t) :: ProjectUser.t
+  @spec associate_invitee(UserInvite.t(), User.t()) :: ProjectUser.t()
   defp associate_invitee(%UserInvite{invitee: nil} = invite, %User{} = user) do
     invite
     |> Changeset.change(%{})
     |> Changeset.put_assoc(:invitee, user)
-    |> Repo.update
+    |> Repo.update()
   end
 
   @spec marshall_response(tuple) :: tuple
