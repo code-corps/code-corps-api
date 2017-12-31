44a66f1301446019526230cb67b8979987e3f8a5
Josh Smith <josh@codecorps.org>
2017-12-31 14:05:08 -0500

    Add alias to InMemoryAPI

--- lib/code_corps/analytics/in_memory_api.ex
+++ lib/code_corps/analytics/in_memory_api.ex
@@ -7,9 +7,15 @@ defmodule CodeCorps.Analytics.InMemoryAPI do
 
   require Logger
 
+  def alias(user_id, previous_id), do: log_alias(user_id, previous_id)
+
   def identify(user_id, _traits), do: log_identify(user_id)
 
   def track(user_id, event_name, properties), do: log_track(user_id, event_name, properties)
+
+  defp log_alias(user_id, previous_id) do
+    Logger.info "Called alias for User #{user_id} with anonymous id #{previous_id}"
+  end
 
   defp log_identify(user_id) do
     Logger.info "Called identify for User #{user_id}"
