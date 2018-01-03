bb1f4c2d89765e1bcdd06d2e2897f40ae66812f4
Nikola Begedin <begedinnikola@gmail.com>
2018-01-03 12:11:22 +0100

    Format CodeCorps.UserInvite

--- lib/code_corps/model/user_invite.ex
+++ lib/code_corps/model/user_invite.ex
@@ -4,13 +4,13 @@ defmodule CodeCorps.UserInvite do
   @type t :: %__MODULE__{}
 
   schema "user_invites" do
-    field :email, :string, null: false
-    field :role, :string
-    field :name, :string
+    field(:email, :string, null: false)
+    field(:role, :string)
+    field(:name, :string)
 
-    belongs_to :project, CodeCorps.Project
-    belongs_to :inviter, CodeCorps.User
-    belongs_to :invitee, CodeCorps.User
+    belongs_to(:project, CodeCorps.Project)
+    belongs_to(:inviter, CodeCorps.User)
+    belongs_to(:invitee, CodeCorps.User)
 
     timestamps()
   end
