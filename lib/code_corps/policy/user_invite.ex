cba0b7a4045741bd3c4a8ce2403017878e183c0e
Nikola Begedin <begedinnikola@gmail.com>
2018-01-03 11:17:54 +0100

    Correct Policy.UserInvite behavior

--- lib/code_corps/policy/user_invite.ex
+++ lib/code_corps/policy/user_invite.ex
@@ -1,7 +1,21 @@
 defmodule CodeCorps.Policy.UserInvite do
+  @moduledoc ~S"""
+  Handles `CodeCorps.User` authorization of actions on `CodeCorps.UserInvite`
+  records.
+  """
+
   alias CodeCorps.{Policy.Helpers, User}
 
-  def create?(%User{} = user, %{"project_id" => _} = params) do
+  @doc ~S"""
+  Returns true if the specified `CodeCorps.User` is allowed to create a
+  `CodeCorps.UserInvite` using the specified attributes.
+  """
+  @spec create?(User.t(), map) :: boolean
+  def create?(
+        %User{id: user_id} = user,
+        %{"project_id" => _, "inviter_id" => inviter_id} = params
+      )
+      when user_id == inviter_id do
     user_role =
       params
       |> Helpers.get_project()
@@ -11,8 +25,14 @@ defmodule CodeCorps.Policy.UserInvite do
     new_role = Map.get(params, "role")
     do_create?(user_role, new_role)
   end
-  def create?(%User{}, %{}), do: true
 
+  def create?(%User{id: user_id}, %{"inviter_id" => inviter_id})
+      when user_id == inviter_id,
+      do: true
+
+  def create?(%User{}, %{}), do: false
+
+  @spec do_create?(String.t() | nil, String.t() | nil) :: boolean
   defp do_create?("admin", "pending"), do: true
   defp do_create?("admin", "contributor"), do: true
   defp do_create?("owner", "pending"), do: true
--- test/lib/code_corps/policy/user_invite_test.exs
+++ test/lib/code_corps/policy/user_invite_test.exs
@@ -9,15 +9,25 @@ defmodule CodeCorps.Policy.UserInviteTest do
       membership |> Changeset.change(%{role: role}) |> Repo.update()
     end
 
-    test "returns true if no project/role specified" do
+    test "returns true if current user matches inviter and no project/role specified" do
       user = insert(:user)
-      assert user |> UserInvite.create?(%{}) == true
+      assert user |> UserInvite.create?(%{"inviter_id" => user.id}) == true
+    end
+
+    test "returns true if current user does not match inviter and no project/role specified" do
+      user = insert(:user)
+      assert user |> UserInvite.create?(%{"inviter_id" => -1}) == false
+    end
+
+    test "returns false if no project/role or inviter specified" do
+      user = insert(:user)
+      assert user |> UserInvite.create?(%{}) == false
     end
 
     test "when invite for project contributor, returns true if user is admin or higher" do
       project = insert(:project)
       user = insert(:user)
-      attrs = %{"project_id" => project.id, "role" => "contributor"}
+      attrs = %{"project_id" => project.id, "role" => "contributor", "inviter_id" => user.id}
 
       refute user |> UserInvite.create?(attrs)
       membership = insert(:project_user, project: project, user: user, role: "pending")
@@ -33,7 +43,7 @@ defmodule CodeCorps.Policy.UserInviteTest do
     test "when invite for project admin, returns true if user is owner" do
       project = insert(:project)
       user = insert(:user)
-      attrs = %{"project_id" => project.id, "role" => "admin"}
+      attrs = %{"project_id" => project.id, "role" => "admin", "inviter_id" => user.id}
 
       refute user |> UserInvite.create?(attrs)
       membership = insert(:project_user, project: project, user: user, role: "pending")
